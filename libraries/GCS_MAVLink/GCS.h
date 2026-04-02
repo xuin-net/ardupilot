759b5b7c683018e6cd2d480686e1f60a087ade6f
xuin <xuin8640@gmail.com>
2026-04-02 11:32:52 +0800

    Update GCS.h

--- libraries/GCS_MAVLink/GCS.h
+++ libraries/GCS_MAVLink/GCS.h
@@ -393,6 +393,8 @@ public:
     void send_set_position_target_global_int(uint8_t target_system, uint8_t target_component, const Location& loc);
     void send_rpm() const;
     void send_generator_status() const;
+
+    void send_wk_selfcheck_state();
 #if AP_WINCH_ENABLED
     virtual void send_winch_status() const {};
 #endif
